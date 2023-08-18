9d9fe3e3b67e2f752c69625cccaef27f09af8e03
Shu Takahashi <shu.takahashi@protonmail.com>
2023-08-19 00:49:10 +0900

    remove oop files

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,15 +1,3 @@
-import {SearchQuery} from './search-query'
-
-export function getAst(input: string) {
-  const sq = new SearchQuery(input)
-  return sq.getAst()
-}
-
-export function getQuery(input: string) {
-  const sq = new SearchQuery(input)
-  return sq.getQuery()
-}
-
 type RegExpGroups<
   TCapture extends string,
   TMaybeCapture extends string = string,
