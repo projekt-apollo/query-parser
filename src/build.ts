f4bc5d0e48f6c548934a5cb4900ee74eebbcab76
Shu Takahashi <shu.takahashi@protonmail.com>
2023-08-18 15:11:50 +0900

    build function

---
+++ src/build.ts
@@ -0,0 +1,64 @@
+import {type Token} from './tokenize'
+
+type QueryFilter = {
+  filter: 'keyword' | 'exact' | string
+  value: string
+}
+export type Query = QueryFilter[]
+
+// warning: function has side effects
+function appendToLastKeyword(query: Query, keyTerm: string) {
+  for (const queryFilter of query.reverse()) {
+    if (queryFilter.filter === 'keyword') {
+      queryFilter.value += ` ${keyTerm}`
+      break
+    }
+  }
+}
+
+export function build(tokens: Token[]) {
+  const query: Query = []
+  let separateNextKeyword = true
+
+  for (const token of tokens) {
+    switch (token.type) {
+      case 'CommaDelimiter': {
+        separateNextKeyword = true
+        break
+      }
+      case 'ExactTerm': {
+        query.push({
+          filter: 'exact',
+          value: token.value,
+        })
+        separateNextKeyword = true
+        break
+      }
+      case 'ColonFilter': {
+        query.push({
+          filter: token.filter.toLowerCase(),
+          value: token.value,
+        })
+        separateNextKeyword = true
+        break
+      }
+      case 'KeywordTerm': {
+        if (separateNextKeyword) {
+          query.push({
+            filter: 'keyword',
+            value: token.value,
+          })
+        } else {
+          appendToLastKeyword(query, token.value)
+        }
+        separateNextKeyword = false
+        break
+      }
+      default: {
+        throw new Error('Cannot reach this part of code')
+      }
+    }
+  }
+
+  return query
+}
---
+++ tests/build.test.ts
@@ -0,0 +1,81 @@
+import {type Token} from '../src/tokenize'
+import {build, type Query} from '../src/build'
+
+test('build empty tokens', () => {
+  const input: Token[] = []
+  const result: Query = []
+
+  expect(build(input)).toEqual(result)
+})
+
+test('ignore comma delimiter', () => {
+  const input: Token[] = [{type: 'CommaDelimiter', value: `,`}]
+  const result: Query = []
+
+  expect(build(input)).toEqual(result)
+})
+
+test('build exact term', () => {
+  const input: Token[] = [{type: 'ExactTerm', value: `regex`}]
+  const result: Query = [{filter: `exact`, value: `regex`}]
+
+  expect(build(input)).toEqual(result)
+})
+
+test('build colon filter', () => {
+  const input: Token[] = [
+    {type: 'ColonFilter', filter: `tag`, value: `typescript`},
+  ]
+  const result: Query = [{filter: `tag`, value: `typescript`}]
+
+  expect(build(input)).toEqual(result)
+})
+
+test('build keyword term', () => {
+  const input: Token[] = [{type: 'KeywordTerm', value: `tea`}]
+  const result: Query = [{filter: `keyword`, value: `tea`}]
+
+  expect(build(input)).toEqual(result)
+})
+
+test('build multiple keyword terms', () => {
+  const input: Token[] = [
+    {type: 'KeywordTerm', value: `tea`},
+    {type: 'KeywordTerm', value: `maccha`},
+  ]
+  const result: Query = [{filter: `keyword`, value: `tea maccha`}]
+
+  expect(build(input)).toEqual(result)
+})
+
+test('build keyword terms delimited by comma delimiter', () => {
+  const input: Token[] = [
+    {type: 'KeywordTerm', value: `tea`},
+    {type: 'CommaDelimiter', value: `,`},
+    {type: 'KeywordTerm', value: `maccha`},
+  ]
+  const result: Query = [
+    {filter: `keyword`, value: `tea`},
+    {filter: `keyword`, value: `maccha`},
+  ]
+
+  expect(build(input)).toEqual(result)
+})
+
+test('build sequence of keyword term, colon filter, keyword term, followed by exact term delimited by comma delimiters', () => {
+  const input: Token[] = [
+    {type: 'KeywordTerm', value: `regex`},
+    {type: 'ColonFilter', filter: `language`, value: `javascript`},
+    {type: 'KeywordTerm', value: `class`},
+    {type: 'CommaDelimiter', value: `,`},
+    {type: 'ExactTerm', value: `types`},
+  ]
+  const result: Query = [
+    {filter: `keyword`, value: `regex`},
+    {filter: `language`, value: `javascript`},
+    {filter: `keyword`, value: `class`},
+    {filter: `exact`, value: `types`},
+  ]
+
+  expect(build(input)).toEqual(result)
+})
